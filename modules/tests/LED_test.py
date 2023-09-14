1da54750b793f6edb0e4895c33492389200028e9
sindrekvande <92534777+sindrekvande@users.noreply.github.com>
2023-09-14 12:00:37 +0200

    sys path for import av LED.py
    Kan sikkert legges en annen plass for at det skal bli en generell løsning.

--- modules/tests/LED_test.py
+++ modules/tests/LED_test.py
@@ -1,5 +1,13 @@
-import LED
 import time
+import sys
+import os
+
+absolute_path = os.path.dirname(__file__)
+relative_path = ".."
+full_path = os.path.join(absolute_path, relative_path)
+sys.path.insert(0, full_path)
+
+import LED
 
 LED = LED.LED()
 
