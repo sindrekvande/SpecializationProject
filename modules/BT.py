f25d55d1ab3e7da96859fa20fcf1bb46703fd944
krissch97 <krissch97@gmail.com>
2023-10-12 14:28:20 +0200

    minor changes

--- modules/BT.py
+++ modules/BT.py
@@ -7,7 +7,7 @@ import parameters as pm
 class BTconnect:
     device = '' #E0:21:78:85:2A:FB
     perfomance = 0
-    testActive = True
+    #testActive = True
 
     async def __aenter__(self):
         await self.scan()
@@ -28,15 +28,16 @@ class BTconnect:
             self.__aenter__()
             #print('Delete paired device on Pi, and/or erase+flash nRF')
 
-    #async def __aexit__(self, exc_type, exc_val, exc_tb):
+    async def __aexit__(self, exc_type, exc_val, exc_tb):
+        pass
     #    self.unpairDevice()
     
     async def find_service(self):
         #async with BleakClient(self.device) as client:
         for service in self.client.services:
             print("{0} : {1}".format(service.uuid, service.description))
-            if True:# pm.SoCservice in service.uuid[:8]:
-                #print('Service {0} found'.format(pm.SoCservice)) # Use this to find the correct service
+            if pm.BTservice in service.uuid[:8]:
+                #print('Service {0} found'.format(pm.BTservice)) # Use this to find the correct service
                 for char in service.characteristics:
                     print(char.properties)
                     '''
@@ -49,30 +50,32 @@ class BTconnect:
                             print('could not read')
                     '''
                     if ('notify' or 'indicate') in char.properties:
-                        try:
-                            await self.client.start_notify(char.uuid, self.callback)
-                            if pm.SoCservice in service.uuid[0:8]: self.serviceChar = char.uuid
-                            print('Waiting for change')
-                            await asyncio.sleep(5.0)
-                            await self.client.stop_notify(char.uuid)
-                        except:
-                            print('could not start notify')
+                        #try:
+                            #await self.client.start_notify(char.uuid, self.callback)
+                        #if pm.BTservice in service.uuid[0:8]:
+                        self.serviceChar = char.uuid
+                            #print('Waiting for change')
+                            #await asyncio.sleep(5.0)
+                            #await self.client.stop_notify(char.uuid)
+                        #except:
+                            #print('could not start notify')
     
     def callback(self, sender: BleakGATTCharacteristic, data: bytearray):
         print(f"{sender}: {data[0]}")
         #Send data[0] to the file
+        self.performance = data[0]
 
     async def scan(self):
         dev = await BleakScanner.discover()
         for i in range(0,len(dev)):
             #print("["+str(i)+"]"+str(dev[i]))
-            if (str(dev[i])[-len(pm.SoCname):] == pm.SoCname):
-                self.device = str(dev[i])[:-(len(pm.SoCname)+2)]
-                print('Device called {0} found with MAC {1}'.format(pm.SoCname,self.device))
+            if (str(dev[i])[-len(pm.BTname):] == pm.BTname):
+                self.device = str(dev[i])[:-(len(pm.BTname)+2)]
+                print('Device called {0} found with MAC {1}'.format(pm.BTname,self.device))
         if self.device == '':
-            print('Device by the name of {0} could not be found'.format(pm.SoCname))
+            print('Device by the name of {0} could not be found'.format(pm.BTname))
 
-    async def waitForDevice(self):
+    async def waitForDevice(self, performance, testActive):
         disconnected_event = asyncio.Event()
         def disconnected_callback(client):
             print("Disconnected event")
@@ -85,6 +88,7 @@ class BTconnect:
                     print("Waiting for disconnect")
                     await disconnected_event.wait()
                     print("Disconnected")
+                    performance = self.perfomance #This only works if there are only ONE notification
                     disconnect_event.clear()
             except:
                 await asyncio.sleep(pm.btInterval)
@@ -135,3 +139,7 @@ class BTconnect:
         p.expect("#")
         p.sendline("quit")
         p.close()
+
+async def BTcorutine(performance, testActive):
+    async with BTconnect() as BT:
+        await BT.waitForDevice(performance, testActive)
